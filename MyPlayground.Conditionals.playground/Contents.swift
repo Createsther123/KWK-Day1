e2ee919e79ba8d7e96a840680878030ed99217d1
Your Name <you@example.com>
2019-08-06 10:34:45 -0400

    Day 1 Lesson

---
+++ MyPlayground.Conditionals.playground/Contents.swift
@@ -0,0 +1,59 @@
+import UIKit
+//
+//var str = "Hello, playground"
+//5 < 3
+//12 > 7
+//6 != 8
+//7 == "7"
+//"karlie" == "karlie"
+//"karlie" = "karliekloss"
+
+//Examples of Conditions
+var dogAge = 12
+if dogAge < 2 {
+    print("You are a puppy🐶")
+}
+if dogAge == 2 {
+print("You are awesome!")
+}
+else if dogAge > 12 {
+    print ("You are elderly.")
+}
+else {
+    print ("You are awesome!")
+}
+
+//Practice - Conditionals
+var favoriteFood = "Pizza"
+if favoriteFood == "Jollof Rice" {
+    print("Jollof Rice is better!")
+}
+else if favoriteFood == "Starbucks" {
+    print("Starbucks is expensive.")
+}
+else if favoriteFood == "Pizza" {
+    print("Pizza is very accessible.")
+}
+
+var hasAccount = true
+if hasAccount == false {
+    print("Please Log in!")
+}
+else if hasAccount == true {
+    print("What is your email?")
+}
+
+var gradeLevel = 5
+if (gradeLevel >= 1) && (gradeLevel <= 5) {
+    print("If you are grades 1 to 5, you are in elementary!")
+}
+else if (gradeLevel >= 6) && (gradeLevel <= 8) {
+    print("If you are in grades 6 to 8, you are in middle school")
+}
+else if (gradeLevel >= 9) && (gradeLevel <= 12) {
+    print
+}
+
+
+
+
